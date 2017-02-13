d53b452268ebf0eee414bb3d7e601465ba4825ed
NTN <ntnguyenit@live.com>
2017-02-13 21:57:41 +0700

    Update theme

--- index.php
+++ index.php
@@ -145,12 +145,6 @@
 
     <?php include "partials/footer.php"?>
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
     <!-- Script to Activate the Carousel -->
     <script>
         $('.carousel').carousel({
